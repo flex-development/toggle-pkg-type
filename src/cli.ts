0d64fcc27aec16714d8353836c872858ba52a69b
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-02-20 19:46:39 -0500

    style(cli): cleanup
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/cli.ts
+++ src/cli.ts
@@ -20,7 +20,7 @@ sade(`${pkg.name.replace(/.*\//, '')} [off|on]`)
   .example('off')
   .example('on')
   .option(
-    '--id, -i',
+    '-i, --id',
     'Module id of package directory or manifest',
     'process.env.npm_package_json'
   )
@@ -28,11 +28,8 @@ sade(`${pkg.name.replace(/.*\//, '')} [off|on]`)
     command: Nilable<Command>,
     flags: mri.Argv<{ id?: string | undefined }>
   ): void {
-    return void toggle(
-      command,
-      flags.id === 'process.env.npm_package_json'
-        ? process.env.npm_package_json
-        : flags.id
-    )
+    const { npm_package_json } = process.env
+    if (flags.id === 'process.env.npm_package_json') flags.id = npm_package_json
+    return void toggle(command, flags.id)
   })
   .parse(process.argv)
